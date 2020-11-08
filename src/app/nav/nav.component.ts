085db0f387cf7b60512e692f78db88b00f6d1239
mingli <mingli2015md@gmail.com>
2020-11-08 15:22:46 +0100

    impl logout and loading indicator

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {isLoggedIn, isLoggedOut} from '../auth/auth.selectors';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {AppState} from '../reducers';
+import {AuthActions} from '../auth/action-types';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  loading = true;
+
+  isLoggedIn$: Observable<boolean>;
+
+  isLoggedOut$: Observable<boolean>;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +26,43 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private router: Router,
+              private store: Store<AppState>) {
+  }
 
+  ngOnInit(): void {
+
+    const userProfile = localStorage.getItem('user');
+
+    if (userProfile) {
+      this.store.dispatch(AuthActions.login({user: JSON.parse(userProfile)}));
+    }
+
+    this.router.events.subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+
+    this.isLoggedIn$ = this.store.select(isLoggedIn);
+    this.isLoggedOut$ = this.store.select(isLoggedOut);
+  }
+
+  logout(): void {
+    this.store.dispatch(AuthActions.logout());
+  }
 }
